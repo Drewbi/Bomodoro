5901b1bbf30f0869bbd926987ac9dc7eee733de2
Drewbi <drewalexander986@gmail.com>
2020-09-19 21:43:03 +0800

    Start on injected code

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({disabled: false, chance: 100}, function() {
+  chrome.storage.local.set({disabled: false, chance: 100}, function() {
     console.log("Background initialised");
   });
+  chrome.tabs.onCreated.addListener(function() {
+    chrome.storage.local.get(['disabled', 'chance'], function(result) {
+      if(!result.disabled && Math.random() * result.chance < 1) console.log('boom') 
+    });
+  })
 });
